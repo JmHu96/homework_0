d41a2429d5699436368a0a0e71ba88e62084cb1a
JmHu96 <90287132+JmHu96@users.noreply.github.com>
2021-10-05 14:46:06 -0500

    commit 1

---
+++ egyptian.py
@@ -0,0 +1,77 @@
+"""
+Egyptian algorithm
+"""
+
+def egyptian_multiplication(a, n):
+    """
+    returns the product a * n
+
+    assume n is a nonegative integer
+    """
+    def isodd(n):
+        """
+        returns True if n is odd
+        """
+        return n & 0x1 == 1
+
+    if n == 1:
+        return a
+    if n == 0:
+        return 0
+
+    if isodd(n):
+        return egyptian_multiplication(a + a, n // 2) + a
+    else:
+        return egyptian_multiplication(a + a, n // 2)
+
+
+if __name__ == '__main__':
+    # this code runs when executed as a script
+    for a in [1,2,3]:
+        for n in [1,2,5,10]:
+            print("{} * {} = {}".format(a, n, egyptian_multiplication(a,n)))
+
+
+def power(a, n):
+    """
+    computes the power a ** n
+
+    assume n is a nonegative integer
+    """
+    
+    if n == 1:
+        return a
+    if n == 0:
+        return 1
+    
+    product = 1
+    for i in range(1,n+1):
+        product = egyptian_multiplication(product,a)
+        
+    return product
+    
+    
+if __name__ == '__main__':
+    # this code runs when executed as a script
+    for a in [3,4,5]:
+        for n in [0,1,3,4,5]:
+            print("{} ** {} = {}".format(a, n, power(a,n)))
+              
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ survey.py
@@ -0,0 +1,12 @@
+"""
+Print answers to the following
+
+1. Jiaming Hu
+2. Master of Computational and Applied Mathematics / 2nd year
+3. Data Science and Machine Learning
+4. Python, R, MATLAB
+5. I first used python in STAT 37710 (Machine Learning), but I had no experience before that.
+6. UTC -5
+7. I want to build good fundation before going into further work in Machine Learning and Data Science.
+8. No.
+"""
