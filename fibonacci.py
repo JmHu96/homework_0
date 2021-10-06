be7af8fc99bede1b1b6aca6c59de8ba72ba9e9ce
JmHu96 <90287132+JmHu96@users.noreply.github.com>
2021-10-05 22:44:18 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -37,33 +37,19 @@ def fibonacci_iter(n):
 
 # Question 3
 def fibonacci_power(n):
-    # This is the same function as in problem 2, but we write its abbreviation as egymul 
-    def egymul(a,n):
-        
-        def isodd(n):
-            return n & 0x1 == 1
-
-        if n == 1:
-            return a
-        if n == 0:
-            return 0
-
-        if isodd(n):
-            return egymul(a + a, n // 2) + a
-        else:
-            return egymul(a + a, n // 2)
-    
     def egyptian_power(n):
         A = np.array([[1,1],[1,0]])      
         B = np.array([[1,0],[0,1]])
         for i in range(n):
-             B[0,0],B[0,1],B[1,0],B[1,1] = [egymul(A[0,0],B[0,0])+egymul(A[0,1],B[1,0]),
-                                            egymul(A[0,0],B[0,1])+egymul(A[0,1],B[1,1]),
-                                            egymul(A[1,0],B[0,0])+egymul(A[1,1],B[1,0]),
-                                            egymul(A[1,0],B[0,1])+egymul(A[1,1],B[1,1])]
+            # Replaced egyptian multiplcation with built in multiplication
+             B[0,0],B[0,1],B[1,0],B[1,1] = [A[0,0]*B[0,0]+A[0,1]*B[1,0],
+                                            A[0,0]*B[0,1]+A[0,1]*B[1,1],
+                                            A[1,0]*B[0,0]+A[1,1]*B[1,0],
+                                            A[1,0]*B[0,1]+A[1,1]*B[1,1]]
             
         return B
     
+    # Everything is the same as above.
     x = np.array([1,0])
     if n == 0:
         return x[1]
@@ -71,7 +57,6 @@ def fibonacci_power(n):
         return x[0]
     
     A = egyptian_power(n-1)
-    #print(A)
     
     return(A[0,0])
     
